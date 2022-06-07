9e7f05e191e339c46f7e5aa0dac6645014ab81e2
jessoy <81160034+jessoy@users.noreply.github.com>
2022-06-07 16:10:23 +0100

    strava data added

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,8 +1,21 @@
 const Activity = (props) => {
+    const { activity } = props
   return (
     <>
-      <h3>I am an activity</h3>
-      {props.activity.name}
+      <h3>{activity.name}</h3>
+      <p>date: {Date(activity.start_date)}</p>
+      <p>name: {activity.athlete.id}</p>
+
+      <p>climb: {activity.total_elevation_gain}m</p>
+
+      <p>time taken: {activity.elapsed_time} seconds</p>
+
+      <p>distance: {activity.distance}m</p>
+
+      <p>average speed: {activity.average_speed}</p>
+
+      <p>cumulative distance</p>
+      <p>cumulative time</p>
     </>
   );
 };
